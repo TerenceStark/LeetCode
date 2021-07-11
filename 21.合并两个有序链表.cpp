c996fdefbafc5ec7cee4ff4c2ca7df05d7033023
TerenceStark <xiashulang@gmail.com>
2021-07-11 20:52:28 +0800

    21.

--- 21.合并两个有序链表.cpp
+++ 21.合并两个有序链表.cpp
@@ -15,11 +15,29 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
+    {
+        ListNode *dummy = new ListNode(-1);
+        ListNode *prev = dummy;
+        while (l1 && l2)
+        {
+            if (l1->val < l2->val)
+            {
+                prev->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                prev->next = l2;
+                l2 = l2->next;
+            }
+            prev = prev->next;
+        }
+        prev->next = l1 ? l1 : l2;
+        return dummy->next;
     }
 };
 // @lc code=end
-
