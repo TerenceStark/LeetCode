3d57396a5ed5e2bd8c6061a9f0af6765522ac945
TerenceStark <xiashulang@gmail.com>
2021-07-28 21:11:32 +0800

    g

---
+++ 52.n皇后-ii.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=52 lang=cpp
+ *
+ * [52] N皇后 II
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int totalNQueens(int n)
+    {
+        g;
+    }
+};
+// @lc code=end
