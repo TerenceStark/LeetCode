521afb1a07ca8c2bb19d66060920385d304aa6e4
TerenceStark <xiashulang@gmail.com>
2021-07-30 00:09:06 +0800

    100

--- 100.相同的树.cpp
+++ 100.相同的树.cpp
@@ -16,7 +16,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution
+class Solution //DFS
 {
 public:
     bool isSameTree(TreeNode *p, TreeNode *q)
@@ -28,4 +28,50 @@ public:
         return q!=nullptr && (p->val == q->val) && isSameTree(p->right, q->right) && isSameTree(p->left, q->left);
     }
 };
+
+/* 
+BFS
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) {
+            return true;
+        } else if (p == nullptr || q == nullptr) {
+            return false;
+        }
+        queue <TreeNode*> queue1, queue2;
+        queue1.push(p);
+        queue2.push(q);
+        while (!queue1.empty() && !queue2.empty()) {
+            auto node1 = queue1.front();
+            queue1.pop();
+            auto node2 = queue2.front();
+            queue2.pop();
+            if (node1->val != node2->val) {
+                return false;
+            }
+            auto left1 = node1->left, right1 = node1->right, left2 = node2->left, right2 = node2->right;
+            if ((left1 == nullptr) ^ (left2 == nullptr)) {
+                return false;
+            }
+            if ((right1 == nullptr) ^ (right2 == nullptr)) {
+                return false;
+            }
+            if (left1 != nullptr) {
+                queue1.push(left1);
+            }
+            if (right1 != nullptr) {
+                queue1.push(right1);
+            }
+            if (left2 != nullptr) {
+                queue2.push(left2);
+            }
+            if (right2 != nullptr) {
+                queue2.push(right2);
+            }
+        }
+        return queue1.empty() && queue2.empty();
+    }
+};
+ */
 // @lc code=end
