bf57501b69210d47f6ef0072b09b1fa667ace109
TerenceStark <xiashulang@gmail.com>
2021-08-14 20:40:59 +0800

    102.

--- code/102.二叉树的层序遍历.cpp
+++ code/102.二叉树的层序遍历.cpp
@@ -34,7 +34,6 @@ public:
         {
             size = nodeQueue.size();
             ans.push_back(vector<int>());
-            
             for (int i = 0; i < size; i++)
             {
                 TreeNode *temp = nodeQueue.front();
