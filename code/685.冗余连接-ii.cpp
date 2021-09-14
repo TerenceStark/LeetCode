69b288236e1536a542ba397b71406f9993019526
TerenceStark <xiashulang@gmail.com>
2021-09-15 00:53:22 +0800

    685.

---
+++ code/685.冗余连接-ii.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=685 lang=cpp
+ *
+ * [685] 冗余连接 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
+
+    }
+};
+// @lc code=end
+
